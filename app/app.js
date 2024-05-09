8fc2c60de8c65b6047e08f4e867fa324b2df7282
unknown <abdullah.mamun.0110@gmail.com>
2024-05-09 21:05:06 +0600

    complete todo app

---
+++ app/app.js
@@ -0,0 +1,259 @@
+import { generateCompleteTodoItemUI, generateTodoItemUI } from "./utils.js";
+
+
+
+//finding todo list from the local storage
+let todoList = JSON.parse(localStorage.getItem("todos"))
+  ? JSON.parse(localStorage.getItem("todos"))
+  : [];
+
+let editId = null;
+
+const completeList = handleDocumentById("complete-list");
+const container = handleDocumentById("main");
+const authenticateBtn = handleDocumentById("auth");
+const isLoggedIn = JSON.parse(localStorage.getItem("isLoggedIn")) || false;
+const form = handleDocumentById("todo-form");
+
+
+function updateTotalTasksCount(domId, taskList) {
+  const element = handleDocumentById(domId);
+  element.innerText = taskList.length;
+}
+
+function handleDocumentById(id) {
+  return document.getElementById(id);
+}
+
+
+
+
+//sort with date
+function sortByDate(e){
+  const ascOrDesc = e.target.value
+  const sortTodo = ascOrDesc === 'asc' ? todoList.sort(sortAsc) : todoList.sort(sortDesc)
+  displayTodoList()
+}
+
+function sortAsc(a, b) {
+  if (a.finishedDate === b.finishedDate) {
+      return a.task.localeCompare(b.task);
+  }
+  return a.finishedDate.localeCompare(b.finishedDate);
+}
+
+function sortDesc(a, b) {
+  if (a.finishedDate === b.finishedDate) {
+      return b.task.localeCompare(a.task);
+  }
+  return b.finishedDate.localeCompare(a.finishedDate);
+}
+
+
+
+
+//show the empty todo message
+function updateEmptyTodoMessage(todoList) {
+  const emptyTodos = handleDocumentById("empty-todos");
+  const todos = todoList.filter((todo) => !todo.complete);
+  const emptyTodoMessage = handleDocumentById("empty-todo-text");
+
+  if (todos.length === 0 && !emptyTodoMessage) {
+    const h1 = document.createElement("h1");
+    h1.innerText = "Todo is empty now!";
+    h1.className = "text-gray-100 text-center";
+    h1.id = "empty-todo-text";
+
+    emptyTodos.appendChild(h1);
+  } else if (todos.length && emptyTodoMessage) {
+    emptyTodoMessage.parentNode.removeChild(emptyTodoMessage);
+  } else return;
+}
+
+
+
+
+//update todo and ui
+function enterEditMode(todoId) {
+  editId = todoId;
+  const findTodo = findTodoById(todoId);
+  const title = handleDocumentById("todo-title");
+  title.value = findTodo.task;
+  title.focus();
+  handleDocumentById("todo-description").value = findTodo.description;
+  handleDocumentById("finished-date").value = findTodo.finishedDate;
+  handleDocumentById("submit").innerText = "update task";
+}
+
+
+
+//render list of todo already complete
+function displayCompleteList() {
+  completeList.innerHTML = "";
+  const completeTodoList = todoList.filter((todo) => todo.complete);
+
+  completeTodoList.forEach((todo) => {
+    generateCompleteTodoItemUI(todo.task, todo.id);
+  });
+
+  const deleteButtons = document.querySelectorAll(".delete-btn");
+  //delete todo listener
+  attachEventListenerToButtons(deleteButtons, deleteTodoById);
+  updateTotalTasksCount("totalCompletedTasks", completeTodoList);
+}
+
+//attach event listener buttons
+function attachEventListenerToButtons(buttons, callback) {
+  buttons.forEach((button) => {
+    button.addEventListener("click", function (event) {
+      const todoId = button.id;
+      callback(todoId);
+    });
+  });
+}
+
+//render todo list
+function displayTodoList() {
+  updateEmptyTodoMessage(todoList);
+  const todoContainer = handleDocumentById("todo-items");
+  todoContainer.innerHTML = ""; // Clear previous list
+  const inCompleteTask = todoList.filter((todo) => !todo.complete);
+  inCompleteTask.forEach((todo) => {
+    generateTodoItemUI(todo);
+  });
+
+  const editButtons = document.querySelectorAll(".edit-btn");
+  const deleteButtons = document.querySelectorAll(".delete-btn");
+  const checkedBtn = document.querySelectorAll(".complete");
+  const selectOption = handleDocumentById('select')
+ 
+  //edit todo listener
+  attachEventListenerToButtons(editButtons, enterEditMode);
+  //delete todo listener
+  attachEventListenerToButtons(deleteButtons, deleteTodoById);
+  //handle the complete task
+  attachEventListenerToButtons(checkedBtn, completeTodoById);
+
+  updateTotalTasksCount("totalTasks", inCompleteTask);
+  //sort listener
+  selectOption.addEventListener('change' , sortByDate)
+  //save on the local storage
+  updateOnLocalStorage();
+}
+
+//set the todoList on localStorage when todo data update
+function updateOnLocalStorage() {
+  const stringifyTodo = JSON.stringify(todoList);
+  localStorage.setItem("todos", stringifyTodo);
+}
+
+//adding todo list
+function addTodoItem(title, description, finishedDate) {
+  const todo = {
+    id: crypto.randomUUID(),
+    description,
+    task: title,
+    date: new Date(),
+    finishedDate,
+    complete: false,
+  };
+  todoList.push(todo);
+  displayTodoList();
+}
+
+function findTodoById(id) {
+  return todoList.find((todo) => todo.id === id);
+}
+
+//update todo
+function updateTodoById(id, title, description, finishedDate) {
+  const findTodo = findTodoById(id);
+  if (!findTodo) return;
+  findTodo.task = title;
+  findTodo.description = description;
+  findTodo.finishedDate = finishedDate;
+  displayTodoList();
+}
+
+//delete todo
+function deleteTodoById(id) {
+  const findTodoIdx = todoList.findIndex((todo) => todo.id === id);
+  if (findTodoIdx === -1) return;
+
+  todoList.splice(findTodoIdx, 1);
+  displayTodoList();
+  displayCompleteList();
+}
+
+function completeTodoById(id) {
+  const findTodo = findTodoById(id);
+  findTodo.complete = true;
+  displayTodoList();
+  displayCompleteList();
+}
+
+function displayInputError() {
+  const todoControl = document.querySelector(".todo-control");
+  const errorSpan = handleDocumentById("error");
+  const span = document.createElement("span");
+  span.id = "error";
+  span.innerText = "invalid form data!";
+  span.className = "text-red-600 my-2 block text-bold";
+
+  if (!errorSpan) todoControl.appendChild(span);
+}
+
+function removeInputError() {
+  const errorSpan = handleDocumentById("error");
+  if (errorSpan) {
+    errorSpan.remove();
+  }
+}
+
+function removeLoginBtnFromDom() {
+  container.classList.remove("hidden");
+  authenticateBtn.remove();
+}
+
+function handleAuth() {
+  localStorage.setItem("isLoggedIn", JSON.stringify(true));
+  removeLoginBtnFromDom();
+}
+
+//handle form
+function handleSubmitForm(e) {
+  e.preventDefault();
+  const formData = new FormData(e.target);
+  const title = formData.get("title");
+  const description = formData.get("description");
+  const finishedDate = formData.get("finishedDate");
+
+  if (title === "" || description === "" || finishedDate === "") {
+    displayInputError();
+    return;
+  }
+
+  if (editId) {
+    updateTodoById(editId, title, description, finishedDate);
+    handleDocumentById('submit').innerText = '+'
+    e.target.reset();
+    return;
+  }
+
+  addTodoItem(title, description, finishedDate);
+  removeInputError();
+  e.target.reset();
+}
+
+//main function
+function main() {
+  if (isLoggedIn) removeLoginBtnFromDom();
+  authenticateBtn.addEventListener("click", handleAuth);
+  displayTodoList();
+  displayCompleteList();
+  //handle button listener
+  form.addEventListener("submit", handleSubmitForm);
+
+}
+
+window.onload = main;
---
+++ app/utils.js
@@ -0,0 +1,55 @@
+
+
+
+//create todo item
+ export function generateTodoItemUI (todo) {
+    const { id, task, description,finishedDate } = todo;
+    const todoContainer = document.getElementById("todo-items");
+    const todoItem = document.createElement("div");
+    todoItem.id = `todo-item-${id}`;
+    todoItem.className =
+      "w-[432px]  h-[85px] bg-[#3E1671] flex flex-col justify-center items-center border border-0 rounded-md my-2";
+    todoItem.innerHTML = `
+        <div class="w-[390px] h-[55px] flex items-center  ">
+          <input class="mt-1 ml-2 h-[19px] complete" type="checkbox" name=""  id="${id}">
+          <div class="w-[330px] overflow-hidden">
+            <div class="flex w-[330px] h-[19px] " >
+            <h2 class="  ps-2 ms-2 text-[#9E78CF] inline-block">${task}</h2>
+            <span class="text-[10px] ps-2 ms-2 text-[#9E78CF] inline-block mt-auto">${finishedDate}</span>
+            
+            </div>
+            <p class="text-[#9E78CF] w-[80%] ps-2 ms-2 text-[12px] py-2"> 
+              ${description}
+            
+            </p>
+          
+          </div>
+          <div class="w-[68px] h-[30px] ml-auto flex justify-between ml-auto">
+            <button class="mr-0 border-0 edit-btn text-[#9E78CF]"  id="${id}">
+              <i class="fa-solid fa-pen-to-square"></i>
+            </button>
+            <button class="ml-0 border-0 delete-btn text-[#9E78CF]" id="${id}">
+              <i class="fa-solid fa-trash"></i>
+            </button>
+          </div>
+        </div>
+      `;
+    todoContainer.appendChild(todoItem);
+  }
+
+
+
+  //display todo list for complete
+export function generateCompleteTodoItemUI(text, id) {
+    const completeList = document.getElementById("complete-list");
+    const div = document.createElement("div");
+    div.className =
+      "w-[432px] h-[75px] bg-[#15101C] border-0 rounded-md flex justify-center my-2 ";
+    div.innerHTML = `
+    <h2 class="text-[#78CFB0] my-auto w-full px-3  line-through">${text}</h2>
+    <button class="mr-8 border-0 delete-btn text-[#9E78CF] " id="${id}" >
+        <i class="fa-solid fa-trash"></i>
+    </button>
+    `;
+    completeList.appendChild(div);
+  }
